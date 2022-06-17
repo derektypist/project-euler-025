39eef91abd146b07e3dc2704b0b30db8a8f9bd5a
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2022-06-17 10:13:37 +0000

    Added clearInfo function at script.js

--- js/script.js
+++ js/script.js
@@ -34,3 +34,9 @@ function digitFibonacci(n) {
 
     return i;
 }
+
+// Function to Clear Information
+function clearInfo() {
+    let txt = "";
+    document.getElementById("numinfo").innerHTML = txt;
+}
